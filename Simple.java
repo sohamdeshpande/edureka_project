010a5f1d6f1fb00d1082d92c5c34846c213ab049
edureka <support@edureka.co>
2022-08-21 03:31:16 +0000

    Revert "Second commit of Simple.java file"
    This reverts commit fbc45e75db2f4b5ae62d4cbcee75f8efc84983bf.

--- Simple.java
+++ Simple.java
@@ -1,6 +1,6 @@
 class Simple{
     public static void main(String args[]){
-    System.out.println("Hello learners, Welcome to Git Online Training");
+    System.out.println("Hello Edureka");
     }
 
 }
